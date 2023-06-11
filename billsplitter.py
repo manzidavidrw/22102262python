0b8f5e314cac0f6efffad12534f733a7096a4251
manzidavidrw <manzi2020d@gmail.com>
2023-06-11 15:21:56 +0200

    splitter

---
+++ billsplitter.py
@@ -0,0 +1,4 @@
+bill = int(input("enter amount of bill"))
+nmbr = int(input("enter number of pepolepaying the bill"))
+split = round(float(bill / nmbr), 2)
+print("amount to be paid by person: ", split)
